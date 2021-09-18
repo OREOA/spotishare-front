dbb0a2b10f9f17571fea29e43a8bea559ca94311
OtsoHellman <35035388+OtsoHellman@users.noreply.github.com>
2021-09-18 22:57:01 +0300

    Update index.tsx

--- src/components/Session/TinderCardComponent/index.tsx
+++ src/components/Session/TinderCardComponent/index.tsx
@@ -24,7 +24,7 @@ const TinderCardComponent = () => {
                     return data
                 })
             return data
-            }).then(data => sendVote(data.id, session.hash))
+            }).then(data => data?.id && sendVote(data.id, session.hash))
     }
 
     return (
